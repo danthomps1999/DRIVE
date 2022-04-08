ca6badee1a0129ca2653362bde1eae1cfc7cb22b
dthomp69@emich.edu <dthomp69@emich.edu>
2022-04-07 21:40:38 -0400

    Some cubeies uwu

--- Assets/Scripts/BuildCar.cs
+++ Assets/Scripts/BuildCar.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class BuildCar : MonoBehaviour
 {
     
-    List<GameObject> cars;
+    List<GameObject> carsForward;
+    List<GameObject> carsBackward;
     public GameObject car;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.cars = new List<GameObject>();
+        this.carsForward = new List<GameObject>();
+        this.carsBackward = new List<GameObject>();
 
-        buildFourMovingCubes();
+        buildManyMovingCubes();
 
         //buildCube();
 
@@ -32,27 +34,46 @@ public class BuildCar : MonoBehaviour
         Instantiate(carCube, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity);
     }
 
-    public void buildFourMovingCubes(){
+    public void buildManyMovingCubes(){
         GameObject carCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         //Would be .5, but the asphalt is .1 up
-        GameObject madeCube0 = Instantiate(carCube, new Vector3(4.0f, .6f, 0.0f), Quaternion.identity);
-        GameObject madeCube1 = Instantiate(carCube, new Vector3(10.0f, .6f, 0.0f), Quaternion.identity);
-        GameObject madeCube2 = Instantiate(carCube, new Vector3(-10.0f, .6f, 0.0f), Quaternion.identity);
-        GameObject madeCube3 = Instantiate(carCube, new Vector3(-4.0f, .6f, 0.0f), Quaternion.identity);
+        GameObject madeCube0 = Instantiate(carCube, new Vector3(2.0f, .6f, 0.0f), Quaternion.identity);
+        GameObject madeCube1 = Instantiate(carCube, new Vector3(4.0f, .6f, 0.0f), Quaternion.identity);
+        GameObject madeCube2 = Instantiate(carCube, new Vector3(6.0f, .6f, 0.0f), Quaternion.identity);
+        GameObject madeCube3 = Instantiate(carCube, new Vector3(8.0f, .6f, 0.0f), Quaternion.identity);
+        GameObject madeCube4 = Instantiate(carCube, new Vector3(10.0f, .6f, 0.0f), Quaternion.identity);
+
+        GameObject madeCube5 = Instantiate(carCube, new Vector3(-10.0f, .6f, 100.0f), Quaternion.identity);
+        GameObject madeCube6 = Instantiate(carCube, new Vector3(-8.0f, .6f, 100.0f), Quaternion.identity);
+        GameObject madeCube7 = Instantiate(carCube, new Vector3(-6.0f, .6f, 100.0f), Quaternion.identity);
+        GameObject madeCube8 = Instantiate(carCube, new Vector3(-4.0f, .6f, 100.0f), Quaternion.identity);
+        GameObject madeCube9 = Instantiate(carCube, new Vector3(-2.0f, .6f, 100.0f), Quaternion.identity);
 
         Destroy(carCube);
         //madeCube.transform.position += Vector3.forward * Time.deltaTime;
-        this.cars.Add(madeCube0);
-        this.cars.Add(madeCube1);
-        this.cars.Add(madeCube2);
-        this.cars.Add(madeCube3);
+        this.carsForward.Add(madeCube0);
+        this.carsForward.Add(madeCube1);
+        this.carsForward.Add(madeCube2);
+        this.carsForward.Add(madeCube3);
+        this.carsForward.Add(madeCube4);
+
+        this.carsBackward.Add(madeCube5);
+        this.carsBackward.Add(madeCube6);
+        this.carsBackward.Add(madeCube7);
+        this.carsBackward.Add(madeCube8);
+        this.carsBackward.Add(madeCube9);
 
     }
 
     public void moveCars(){
-        foreach (GameObject car in this.cars)
+        foreach (GameObject car in this.carsForward)
         {
             car.transform.position += Vector3.forward * Time.deltaTime * 10;
+        }
+
+        foreach (GameObject car in this.carsBackward)
+        {
+            car.transform.position += Vector3.forward * Time.deltaTime * -10;
         }
     }
 }
